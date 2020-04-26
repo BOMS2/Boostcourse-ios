12fb0296115ba3aba05b2bf0287559d4cecc0b0f
bomin <kbm97121@gmail.com>
2020-04-26 13:02:30 +0900

    add button, slider IBAction code

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	@IBOutlet weak var playPauseButton: UIButton!
+	
+	@IBOutlet weak var timeLabel: UILabel!
+	
+	@IBOutlet weak var progressSlider: UISlider!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+		
+	}
+	
+	
+	@IBAction func sliderValueChangedOne(_ sender: UISlider) {
+		
+	}
+	
 }
 
